773881b31a139ea12bdc757ca0ab0ee4e32092c2
Hongbo Zhu <49983377+DonovanZhu@users.noreply.github.com>
2021-05-10 22:41:07 -0400

    Update recv.cpp

--- Motor_Control/PC/recv.cpp
+++ Motor_Control/PC/recv.cpp
@@ -15,15 +15,15 @@ using namespace std;
 // Using this class to hold data from RPi
 class MsgToPC {
 public:
-	float joint_pos[MOTOR_NUM]; 	// Rotation angle, unit rad
-	float joint_vel[MOTOR_NUM]; 	// Rotation speed, unit rad/s
-	float joint_cur[MOTOR_NUM]; 	// Rotation current, unit A
+	float joint_pos[MOTOR_NUM]; 	// Rotation angle, unit degree
+	float joint_vel[MOTOR_NUM]; 		// Rotation speed, unit rpm
+	float joint_cur[MOTOR_NUM]; 	// Rotation torque, unit N*m
 	float acc[3];		// Acceleration of IMU, unit m/s^2
-	float gyr[3];		// Gyroscope, unit rad/s
-	float mag[3];		// Magnetometer, unit 
-	float euler[3];
+	float gyr[3];		// Gyroscope, unit deg/s
+	float mag[3];
+	float eular[3];
 	float timestamps;
-	MSGPACK_DEFINE(joint_pos, joint_vel, joint_cur, acc, gyr, mag, euler, timestamps);
+	MSGPACK_DEFINE(joint_pos, joint_vel, joint_cur, acc, gyr, mag, eular, timestamps);
 };
 
 void main()
@@ -78,16 +78,16 @@ void main()
 		// Save data into txt file
 		/*
 		file << recv[0].timestamps << "\t";
-		file << recv[0].joint_pos[0] << "\t" << recv[0].joint_pos[1] << "\t" << recv[0].joint_pos[2] << "\t" << recv[0].joint_pos[3] << "\t";
+		file << recv[0].angle[0] << "\t" << recv[0].angle[1] << "\t" << recv[0].angle[2] << "\t" << recv[0].angle[3] << "\t";
 		file << recv[0].rpm[0] << "\t" << recv[0].rpm[1] << "\t" << recv[0].rpm[2] << "\t" << recv[0].rpm[3] << "\t";
-		file << recv[0].joint_cur[0] << "\t" << recv[0].joint_cur[1] << "\t" << recv[0].joint_cur[2] << "\t" << recv[0].joint_cur[3] << "\t";
+		file << recv[0].torque[0] << "\t" << recv[0].torque[1] << "\t" << recv[0].torque[2] << "\t" << recv[0].torque[3] << "\t";
 		file << recv[0].command[0] << "\t" << recv[0].command[1] << "\t" << recv[0].command[2] << "\t" << recv[0].command[3] << "\t";
 		file << recv[0].acc[0] << "\t" << recv[0].acc[1] << "\t" << recv[0].acc[2] << "\t";
 		file << recv[0].gyr[0] << "\t" << recv[0].gyr[1] << "\t" << recv[0].gyr[2] << "\t";
 		file << recv[0].mag[0] << "\t" << recv[0].mag[1] << "\t" << recv[0].mag[2] << "\t";
-		file << recv[0].euler[0] << "\t" << recv[0].euler[1] << "\t" << recv[0].euler[2] << "\n";
+		file << recv[0].eular[0] << "\t" << recv[0].eular[1] << "\t" << recv[0].eular[2] << "\n";
 		*/
-		printf("%f\t%f\t%f\n", recv.euler[0], recv.euler[1], recv.euler[2]);
+		printf("%f\t%f\t%f\n", recv.eular[0], recv.eular[1], recv.eular[2]);
 		//printf("%f\n", recv[0].timestamps);
 	}
 	// Close file
